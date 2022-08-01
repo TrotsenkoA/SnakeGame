792c1270efcd8a48eec29ef5cc1ecc9f9c26ea54
Anton Trotsenko <in.lake.district@gmail.com>
2022-08-01 17:50:58 +0300

    Add project files.

---
+++ SnakeGame/Pixel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace SnakeGame
+{
+    public readonly struct Pixel
+    {
+        private const char PixelChar = '█';
+        public Pixel(int x, int y, ConsoleColor color) 
+        {
+            X = x;
+            Y = y;
+            Color = color;
+        }
+
+        public int X { get; }
+
+        public int Y { get; }
+
+        public ConsoleColor Color { get; }
+
+        public void Draw ()
+        {
+            Console.SetCursorPosition(left: X, top: Y);
+            Console.Write(PixelChar); 
+        }
+        public void Clear()
+        {
+            Console.SetCursorPosition(left: X, top: Y);
+            Console.Write(' ');
+        }
+
+    }
+}
---
+++ SnakeGame/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+
+namespace SnakeGame
+{
+    class Program
+    {
+        private const int MapWidth = 30;
+        private const int MapHeight = 20;
+
+        private const ConsoleColor BorderColor = ConsoleColor.Gray;
+            static void Main()
+        {
+            SetWindowSize(MapWidth, MapHeight);
+            SetBufferSize(MapWidth, MapHeight);
+            CursorVisible = false;
+
+            DrawBorder();
+
+            ReadKey();
+                    
+        }
+
+        static void DrawBorder()
+        {
+            for (int i = 0; i < MapWidth; i++)
+            {
+                new Pixel(x: i, y: 1, BorderColor).Draw();
+                new Pixel(x: i, y: MapHeight - 1, BorderColor).Draw();
+            }
+
+            //for (int i = 0; i < MapHeight; i++)
+            //{
+            //    new Pixel(x: 0, y: i, BorderColor).Draw();
+            //    new Pixel(x: MapWidth - 1, y: i, BorderColor).Draw();
+            //}
+        }
+    }
+}
